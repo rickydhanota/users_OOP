2408bcd52623b5139fdff8f2d7848caaa6e266f3
Ricky <rickdhanota@gmail.com>
2020-08-09 01:17:05 -0700

    added the bankaccount class, and made it so you could deposit money both ways

--- users.py
+++ users.py
@@ -1,71 +1,68 @@
-class Users:
-    def __init__(self,name, email):
-        self.name = name
-        self.email = email
-        self.account_balance=0
-
-    def make_withdrawl(self, amount):
-        if self.account_balance<amount:
-            self.account_balance -= 5
-            print('Insufficient funds: Charging a $5 fee')
-        else:
-            self.account_balance -= amount
-            return self
-    
-    def make_deposit(self, amount):
-        self.account_balance += amount
-        return self
-
-    def display_user_balance(self):
-        print(f'User:{self.name}, Account Balance = {self.account_balance}')
-        return self
-    
-    def transfer_money(self, other_user, amount):
-        other_user.account_balance -= amount
-        self.account_balance += amount
-        print(f'User: {self.name}, Account Balance: {self.account_balance}')
-        print(f'{other_user.name} is transfering {amount} dollars to {self.name}. {other_user.name}s balance is now {other_user.account_balance}')
-        return self
-
+#the class BankAccount needs to come first because python reads top down, and also BankAccount is called within the Class Users. It behaves like a function where you can call a function within a function
 class BankAccount:
-    def __init__(self, int_rate=0, balance=0):
+    def __init__(self, int_rate, balance): 
         self.int_rate = int_rate
-        self.balance = balance
-    
+        self.interest_earned = 0
+        if balance>0:
+            self.balance = balance
+        else:
+            self.balance=0
+
     def deposit(self, amount):
         self.balance += amount
         return self
-    
+
     def withdraw(self, amount):
-        if self.balance < amount:
+        if self.balance <= amount:
             self.balance -= 5
-            print('Insufficient funds: Charging a $5 fee')
+            print("Your balance is low, charging a $5 fee")
         else:
             self.balance -= amount
         return self
-    
-    def display_account_info(self):
-        print(f'Account Balance = {self.balance}')
-        return self
-    
+
     def yield_interest(self):
-        if self.balance > 0:
-            self.balance = self.balance + self.balance * self.int_rate
+        self.interest_earned += ((self.int_rate/100)*self.balance)
+        # print(f"Your interest rate is {self.int_rate}, and your interest earned is {self.interest_earned}")
         return self
 
-Ricky=Users("Ricky Dhanota", "rickdhanota@gmail.com")
-Sukhpreet=Users("Sukhpreet Gill", "preetxgill@gmail.com")
-Cleo = Users("Cleo Gill", "meowxgill@gmail.com")
+    def display_account_info(self):
+        print(f"You have an interest rate of {self.int_rate}%, with an account balance of ${self.balance}. The amount of interest earned is ${self.interest_earned}")
+        return self
 
-# Ricky.make_deposit(200).make_deposit(100).make_deposit(300).make_withdrawl(50).display_user_balance()
-# Sukhpreet.make_deposit(200).make_deposit(200).make_deposit(370).make_withdrawl(120).display_user_balance()
+class Users:
+    def __init__(self, name, email, int_rate, balance):
+        self.name=name
+        self.email=email
+        self.account = BankAccount(int_rate,balance)
 
-# Cleo.transfer_money(Sukhpreet, 100)
-# Sukhpreet.display_user_balance()
+    def make_deposit(self, amount):
+        self.account.balance = self.account.balance + amount
+        print("Deposited:",amount)
+        return self
 
-ba1 = BankAccount(balance=100)
-ba2 = BankAccount(.01, 500)
+    def make_withdrawal(self, amount):
+        self.account.balance = self.account.balance - amount
+        print("Withdrawn:", amount)
+        return self
 
-ba1.deposit(100).deposit(50).deposit(50).withdraw(100).yield_interest().display_account_info()
-ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(50).withdraw(50).yield_interest().display_account_info()
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account.balance}"'\n')
+        return self
+
+# Ricky = BankAccount(.3, 1000)
+# Sukhpreet = BankAccount(.43, 2000)
+
+Ricky = Users("Ricky Dhanota", "rickdhanota@gmail.com", .3, 1000)
+Sukhpreet = Users("Sukhpreet Gill", "preetxgill@gmail.com", .43, 2000)
+
+
+Ricky.make_deposit(800).make_deposit(350).make_deposit(270).make_withdrawal(400).display_user_balance()
+
+Sukhpreet.make_deposit(1000).make_deposit(600).make_withdrawal(800).make_withdrawal(100).display_user_balance()
+
+Sukhpreet.account.yield_interest().display_account_info()
+
+Ricky.account.deposit(100).deposit(300).display_account_info() #This is doing the deposit method from within the class BankAccount
+
+
 
